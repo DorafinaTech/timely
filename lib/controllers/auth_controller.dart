cb8cf5e7f821d6d39842d15d62cfd6462efdbe56
danroyal001 <danroyaleffiong@gmail.com>
2023-02-21 15:22:33 +0100

    _

--- lib/controllers/auth_controller.dart
+++ lib/controllers/auth_controller.dart
@@ -58,9 +58,11 @@ class AuthController extends GetxController {
       // Trigger the authentication flow
       final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
 
+      print(googleUser);
+
       // Obtain the auth details from the request
       final GoogleSignInAuthentication? googleAuth =
-          await googleUser?.authentication;
+          await googleUser?.authentication!;
 
       // Create a new credential
       final credential = GoogleAuthProvider.credential(
