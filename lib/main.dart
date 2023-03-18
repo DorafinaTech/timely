6d4935c55f5733aa30c29a49878ce4b902b17059
danroyal001 <danroyaleffiong@gmail.com>
2023-03-18 15:46:19 +0100

    minor changes

--- lib/main.dart
+++ lib/main.dart
@@ -25,14 +25,14 @@ import 'package:timely/views/recover_password.dart';
 import 'package:timely/views/register.dart';
 import 'package:timely/views/splash_screen.dart';
 import 'package:timely/views/test_screen.dart';
-
-import 'constants/primary_color.dart';
+import 'package:timely/constants/primary_color.dart';
+import 'package:loader_overlay/loader_overlay.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   usePathUrlStrategy();
 
-  var app = await Firebase.initializeApp(
+  await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
 
@@ -46,153 +46,155 @@ class TimelyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GetMaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Timely',
-      initialRoute: RoutePaths.splash,
-      getPages: [
-        GetPage(
-          title: RouteNames.splash,
-          name: RoutePaths.splash,
-          page: () => splashScreen,
-        ),
-        GetPage(
-          title: RouteNames.onboarding1,
-          name: RoutePaths.onboarding1,
-          page: () => const Onboarding1(),
-        ),
-        GetPage(
-          title: RouteNames.onboarding2,
-          name: RoutePaths.onboarding2,
-          page: () => const Onboarding2(),
-        ),
-        GetPage(
-          name: RoutePaths.onboarding3,
-          title: RouteNames.onboarding3,
-          page: () {
-            return const Onboarding();
-          },
-        ),
-        GetPage(
-          name: RoutePaths.homeScreen,
-          title: RouteNames.homeScreen,
-          page: () {
-            return HomeScreen();
-          },
-        ),
-        GetPage(
-          name: RoutePaths.login,
-          title: RouteNames.login,
-          page: () {
-            return const Login();
-          },
-        ),
-        GetPage(
-          name: RoutePaths.forgotPassword,
-          title: RouteNames.forgotPassword,
-          page: () {
-            return const RecoveryPassword();
-          },
-        ),
-        GetPage(
-          name: RoutePaths.resetPasswordViaEmail,
-          title: RouteNames.resetPasswordViaEmail,
-          page: () {
-            return const EmailRecovery();
-          },
-        ),
-        GetPage(
-          name: RoutePaths.register,
-          title: RouteNames.register,
-          page: () {
-            return const Register();
-          },
-        ),
-        GetPage(
-          name: RoutePaths.examsScreen,
-          title: RouteNames.examsScreen,
-          page: () {
-            return ExamScreen();
-          },
-        ),
-        GetPage(
-          name: RoutePaths.testScreen,
-          title: RouteNames.testScreen,
-          page: () {
-            return TestScreen();
-          },
-        ),
-        GetPage(
-          name: RoutePaths.readingScreen,
-          title: RouteNames.readingScreen,
-          page: () {
-            return ReadingScreen();
-          },
-        ),
-        GetPage(
-          name: RoutePaths.profileScreen,
-          title: RouteNames.profileScreen,
-          page: () {
-            return const ProfileScreen();
-          },
-        ),
-        GetPage(
-          name: RoutePaths.editProfileScreen,
-          title: RouteNames.editProfileScreen,
-          page: () {
-            return const EditProfileScreen();
-          },
-        ),
-        GetPage(
-          name: RoutePaths.calendarScreen,
-          title: RouteNames.calenderscreen,
-          page: () {
-            return CalendarScreen();
-          },
-        ),
-        GetPage(
-          name: RoutePaths.notescreen,
-          title: RouteNames.notescreen,
-          page: () {
-            return Notes();
-          },
-        ),
-        GetPage(
-          name: RoutePaths.newNoteScreen,
-          title: RouteNames.newNoteScreen,
-          page: () {
-            return NewNoteScreen();
-          },
-        ),
-        GetPage(
-          name: RoutePaths.aboutscreen,
-          title: RouteNames.aboutscreen,
-          page: () {
-            return const About();
-          },
-        ),
-        GetPage(
-          name: RoutePaths.confirmPasswordScreen,
-          title: RouteNames.confirmPasswordScreen,
-          page: () {
-            return ConfirmPassword();
-          },
-        ),
-        GetPage(
-          name: RoutePaths.emailecoveryScreen,
-          title: RouteNames.emailecoveryScreen,
-          page: () {
-            return const EmailRecovery();
-          },
-        ),
-      ],
-      theme: ThemeData(
-          useMaterial3: true,
-          primaryColor: Color(primaryColorCode),
-          colorScheme: ColorScheme.fromSwatch(
-                  primarySwatch: getPrimarySwatch(Color(primaryColorCode)))
-              .copyWith(background: Colors.white)),
-      themeMode: ThemeMode.light,
+    return GlobalLoaderOverlay(
+      child: GetMaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Timely',
+        initialRoute: RoutePaths.splash,
+        getPages: [
+          GetPage(
+            title: RouteNames.splash,
+            name: RoutePaths.splash,
+            page: () => splashScreen,
+          ),
+          GetPage(
+            title: RouteNames.onboarding1,
+            name: RoutePaths.onboarding1,
+            page: () => const Onboarding1(),
+          ),
+          GetPage(
+            title: RouteNames.onboarding2,
+            name: RoutePaths.onboarding2,
+            page: () => const Onboarding2(),
+          ),
+          GetPage(
+            name: RoutePaths.onboarding3,
+            title: RouteNames.onboarding3,
+            page: () {
+              return const Onboarding();
+            },
+          ),
+          GetPage(
+            name: RoutePaths.homeScreen,
+            title: RouteNames.homeScreen,
+            page: () {
+              return HomeScreen();
+            },
+          ),
+          GetPage(
+            name: RoutePaths.login,
+            title: RouteNames.login,
+            page: () {
+              return const Login();
+            },
+          ),
+          GetPage(
+            name: RoutePaths.forgotPassword,
+            title: RouteNames.forgotPassword,
+            page: () {
+              return const RecoveryPassword();
+            },
+          ),
+          GetPage(
+            name: RoutePaths.resetPasswordViaEmail,
+            title: RouteNames.resetPasswordViaEmail,
+            page: () {
+              return const EmailRecovery();
+            },
+          ),
+          GetPage(
+            name: RoutePaths.register,
+            title: RouteNames.register,
+            page: () {
+              return const Register();
+            },
+          ),
+          GetPage(
+            name: RoutePaths.examsScreen,
+            title: RouteNames.examsScreen,
+            page: () {
+              return ExamScreen();
+            },
+          ),
+          GetPage(
+            name: RoutePaths.testScreen,
+            title: RouteNames.testScreen,
+            page: () {
+              return TestScreen();
+            },
+          ),
+          GetPage(
+            name: RoutePaths.readingScreen,
+            title: RouteNames.readingScreen,
+            page: () {
+              return ReadingScreen();
+            },
+          ),
+          GetPage(
+            name: RoutePaths.profileScreen,
+            title: RouteNames.profileScreen,
+            page: () {
+              return const ProfileScreen();
+            },
+          ),
+          GetPage(
+            name: RoutePaths.editProfileScreen,
+            title: RouteNames.editProfileScreen,
+            page: () {
+              return const EditProfileScreen();
+            },
+          ),
+          GetPage(
+            name: RoutePaths.calendarScreen,
+            title: RouteNames.calenderscreen,
+            page: () {
+              return CalendarScreen();
+            },
+          ),
+          GetPage(
+            name: RoutePaths.notescreen,
+            title: RouteNames.notescreen,
+            page: () {
+              return Notes();
+            },
+          ),
+          GetPage(
+            name: RoutePaths.newNoteScreen,
+            title: RouteNames.newNoteScreen,
+            page: () {
+              return NewNoteScreen();
+            },
+          ),
+          GetPage(
+            name: RoutePaths.aboutscreen,
+            title: RouteNames.aboutscreen,
+            page: () {
+              return const About();
+            },
+          ),
+          GetPage(
+            name: RoutePaths.confirmPasswordScreen,
+            title: RouteNames.confirmPasswordScreen,
+            page: () {
+              return ConfirmPassword();
+            },
+          ),
+          GetPage(
+            name: RoutePaths.emailecoveryScreen,
+            title: RouteNames.emailecoveryScreen,
+            page: () {
+              return const EmailRecovery();
+            },
+          ),
+        ],
+        theme: ThemeData(
+            useMaterial3: true,
+            primaryColor: Color(primaryColorCode),
+            colorScheme: ColorScheme.fromSwatch(
+                    primarySwatch: getPrimarySwatch(Color(primaryColorCode)))
+                .copyWith(background: Colors.white)),
+        themeMode: ThemeMode.light,
+      ),
     );
   }
 }
