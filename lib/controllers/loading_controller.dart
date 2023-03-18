43931af6089392ea42fe53d4833ea74a442b8c29
danroyal001 <danroyaleffiong@gmail.com>
2023-03-18 15:59:08 +0100

    _

--- lib/controllers/loading_controller.dart
+++ lib/controllers/loading_controller.dart
@@ -1,15 +1,20 @@
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:timely/controllers/base_controller.dart';
 import 'package:loader_overlay/loader_overlay.dart';
 
 class LoadingControler extends BaseController {
   void startLoading() {
-    Get.context.loaderOverlay.show();
+    Get.context?.loaderOverlay.show();
   }
 
   void stopLoading() {
-    if (context.loaderOverlay.visible) {
-      context.loaderOverlay.hide();
+    try {
+      if (Get.context!.loaderOverlay.visible) {
+        Get.context?.loaderOverlay.hide();
+      }
+    } catch (e) {
+      debugPrint(e.toString());
     }
   }
 }
